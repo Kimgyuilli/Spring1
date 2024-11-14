c76dc68a4c69f76d47ac8843aca61594970aafc0
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2024-11-14 20:10:26 +0900

    feat: web

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+    private MemberService memberService;
+
+/*    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }*/
 
     @Autowired
     public MemberController(MemberService memberService) {
