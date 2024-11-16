23c684d739bcb6c968cd9e534ae69f4a7af3524e
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2024-11-16 23:36:20 +0900

    feat: web

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hello_spring.controller;
+
+public class MemberForm {
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+}
